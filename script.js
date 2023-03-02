b07653649faff26c2deac24cf83d31f2ee400a63
Travis Nicholson <110001213+Travisnicholson90@users.noreply.github.com>
2023-03-02 16:39:43 +1100

    readme-updates

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const button = document.querySelector('#generate');
 
 // generate password button. initiated the password prompts 
 button.addEventListener('click', (e) => {
-  generatePassword();
+  
+  
+    generatePassword();
+
+  
+ 
   
 })
   
@@ -18,14 +23,16 @@ let symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '?', '<', '>']
 
 //password criteria prompts
 let passwordLength = prompt('Please enter a password length, between 8 - 128 characters');
-let uppercase = confirm('Do you want to include uppercase characters?');
-let number = confirm('Do you want to include numbers in your password');
-let lowercase = confirm('Do you want to include lowercase characters?');
-let specialCharacters = confirm('Do you want to include special characters?');
-
-//empty array to push password inputs
+ 
+  let uppercase = confirm('Do you want to include uppercase characters?');
+  let number = confirm('Do you want to include numbers in your password');
+  let lowercase = confirm('Do you want to include lowercase characters?');
+  let specialCharacters = confirm('Do you want to include special characters?');
+  
+  
+  //empty array to push password inputs
   let merge = []
-//prompts === true are pushed to the merge array using the spread operator
+  //prompts === true are pushed to the merge array using the spread operator
   if(number) { 
     merge.push(...numbers)
   } 
@@ -38,22 +45,24 @@ let specialCharacters = confirm('Do you want to include special characters?');
   if(specialCharacters) {
     merge.push(...symbols)
   }
-    //console.log(merge);
-
-const newPassword = [];
-const password = [];
-
+  //console.log(merge);
+  
+  const newPassword = [];
+  const password = [];
+  
   for(let i = 0; i < passwordLength; i++) {
-
+    
     const random = Math.floor(Math.random() * (merge.length));
     newPassword.push(merge[random]);
-
+    
   }
   password.push(newPassword.join(' '));
   
-
+  
   const textField = document.querySelector('#password');
   textField.innerHTML = password;
   console.log(password);
+  
+} 
 
-}
+
